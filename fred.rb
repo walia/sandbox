ce6b5f01baccd587a2a1f132d296bbd19f19331d
Messay Bekele <mabekele@gmail.com>
2014-03-23 13:13:36 -0400

    adding age for fred.

--- fred.rb
+++ fred.rb
@@ -1,7 +1,11 @@
 class Fred
-  attr_accessor :occupation
+  attr_accessor :occupation, :age
   
   def initialize(v)
     @occupation = v
+  end
+
+  def to_s
+    "Fred is an #{occupation} and #{age} years old."
   end
 end
